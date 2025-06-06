fa9a31f32f11b885ec0767853b6e946c32fe2a7e
ITEAA <hanta67@naver.com>
2025-06-06 23:14:51 +0900

    Update save_jobs.py

--- save_jobs.py
+++ save_jobs.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://118.67.151.173/data/api/jopblancApi.do"
+url = "http://127.0.0.1:5001/proxy"
 params = {
     "authKey": "fYL5gLDcPZ/iE6TB7Rmg1AnxisbHUUFMUuK8Am/MxcIC5+G2awO4kGH6CjFbgwAorXjRlhuqogcHGSEyLzQXdoOW2XonGbNFkASwL8QBm6FkiXgC/hHz+Jr/HAInzOPG",
     "callTp": "L",
@@ -9,7 +9,4 @@ params = {
 }
 
 response = requests.get(url, params=params, verify=False)
-
-with open("job_data.xml", "w", encoding="utf-8") as f:
-    f.write(response.text)
-print("✅ API 응답 저장 완료: job_data.xml")
+print(response.text)
